418a0dff82b2ff248e0d8e4ba2441b33d3148442
raisameneses <raisameneses1@gmail.com>
2018-01-29 13:04:35 -0800

    Vehicle moving, torque applied. Physics needs update

--- AutonomousVehicles.js
+++ AutonomousVehicles.js
@@ -1,34 +1,77 @@
 
+
+
 //used for vehicles starting position 
 function randomInt(n) {
   return Math.floor(Math.random() * n);
 }
-//a Vector directs vehicle's movements and direction 
-//as well as the target's location.  
+/***********************************************************************
+ * Vector
+ **********************************************************************/
 var Vector = function (x, y) {
-  this.vectorX = x,
+    this.vectorX = x,
     this.vectorY = y
 }
 Vector.prototype.addVector = function (v2) {
-  this.x += v2.x;
-  this.y += v2.y;
+  this.vectorX += v2.vectorX;
+  this.vectorY += v2.vectorY;
+}
+
+Vector.prototype.addValue = function (value) {
+  this.vectorX += value;
+  this.vectorY += value;
 }
 Vector.prototype.subtractVector = function (v2) {
-  this.x -= v2.x;
-  this.y -= v2.y;
+  this.vectorX -= v2.vectorX;
+  this.vectorY -= v2.vectorY;
 }
-//Calculates the distance between vectors
+//Multiplies vector by another vector
+Vector.prototype.MultiplyVector = function (v2) {
+  this.vectorX *= v2.vectorX;
+  this.vectorY *= v2.vectorY;
+}
+//Multiplies vector by a number, not another vector
+Vector.prototype.Multiply = function (value) {
+  this.vectorX *= value;
+  this.vectorY *= value;
+}
+//Distance formula
 Vector.prototype.distance = function (v2) {
-  var pow1 = Math.pow((this.x - v2.x), 2);
-  var pow2 = Math.pow((this.y - v2.y), 2);
+  var pow1 = Math.pow((this.vectorX - v2.vectorX), 2);
+  var pow2 = Math.pow((this.vectorY - v2.vectorY), 2);
   return Math.sqrt(pow1 + pow2);
 }
 
-//Blueprint for a vehicle. Contains all the body information
+//Vector's magnitude, used to normalize vector
+Vector.prototype.magnitude = function(){
+  return Math.sqrt((this.vectorX * this.vectorX) + (this.vectorY * this.vectorY)); 
+}
+
+Vector.prototype.Sqmg = function(){
+  return (this.vectorX * this.vectorX) + (this.vectorY * this.vectorY); 
+}
+Vector.prototype.Limit = function(max){
+  if(this.Sqmg > max * max){
+    this.normalize();
+    this.Multiply(max); 
+  }
+}
+
+Vector.prototype.normalize = function (){
+  var mag = this.magnitude();
+  if(mag > 0){
+    this.vectorX = (this.vectorX / mag); 
+    this.vectorY = (this.vectorY / mag); 
+  }
+}
+
+/***********************************************************************
+ * Circle
+ **********************************************************************/
 var Circle = function (centerX, centerY, circleRadius) {
   this.center = new Vector(centerX, centerY);
   this.radius = circleRadius;
-  this.mass - 10;  //Test different numbers.. this can be passed. TO IMPROVE. 
+  this.mass = 10;  //Test different numbers.. this can be passed. TO IMPROVE. 
   this.momentOfInertia = this.getCenterOfInertia();
 }
 //sum of Mass * r^2 calculated based on moment of inertia for a circle
@@ -38,10 +81,14 @@ Circle.prototype.getCenterOfInertia = function () {
   return mInertia;
 }
 
+/***********************************************************************
+ * Light
+ **********************************************************************/
 var LightSource = function(game){  
   this.ctx = game.ctx;
   this.radius = 40;
   this.lightLocation = new Vector((game.ctx.canvas.width / 2), (game.ctx.canvas.height / 2));
+  this.luminosity = 10; //Constant used to calculate light brightness with respect to each wheel.
 }
 
 LightSource.prototype.draw = function() {
@@ -56,81 +103,86 @@ LightSource.prototype.update = function() {
  //Do nothing, values are stable. 
 }
 
-LightSource.prototype.getLocation = function(){
-  return this.lightLocation;
-}
 
-//Vehicle. Uses phenotype of a circle. Makes decisions based on physics laws. 
-var AutonomousVehicle = function (game, light) {
-  this.ctx = game.ctx;
-  this.locationGoal = light.getLocation();
-  this.direction = this.whereAmIGoing(); 
+
+/***********************************************************************
+ * Vehicle
+ **********************************************************************/
+var AutonomousVehicle = function (game, light) {  
+  this.game = game;     
+  this.ctx = game.ctx; 
+  this.lightSource = light;
   let x = randomInt(800); 
-  let y = randomInt(800); 
-  this.game = game;
+  let y = randomInt(800);   
+  this.angle = 90;  
   this.radius  = ((game.ctx.canvas.height) / 50);
-  this.circle = new Circle(x, y, this.radius );  //Update the circle in every iteration
-  this.speed = 5;
-  const angle = 90;
-  this.torque = 0.0; //rotation angle in radians 
-  this.direction = (this.circle.center.vectorY + (-this.radius ));   //Starts going to the up
-  
+  this.position = new Circle(x, y, this.radius );  //Update the circle in every iteration
+  this.rightSensor = this.PointInVehicle(60);  
+  this.leftSensor = this.PointInVehicle(120);
+  this.direction = this.PointInVehicle(this.angle); //Start position = going north. Adapt line draing using this value
+  this.rightWheel = 0.0;
+  this.leftWheel = 0.0; 
+  this.acceleration = 2;
+  this.force = 0.0;
+  this.torque = 0.0; //rotation angle in radians  
 }
 
-//returns true if the right sensor is false, left otherwise
-AutonomousVehicle.prototype.CloserSensorToLight = function () {
-  // var leftSensor = new Vector((this.circle.center.vectorX -  this.radius), this.circle.center.vectorY);
-  // console.log("Left Sensor " + leftSensor.center)
-  // var left =  leftSensor.distance(this.locationGoal);
-  // console.log(left)
-
-  // var rightSensor = new Vector((this.circle.center.vectorX +  this.radius), this.circle.center.vectorY);
-  // console.log("right Sensor " + rightSensor.center)
-  // var right = rightSensor.distance(this.locationGoal);
-  // console.log(right)
-  // if (left < right) {
-  //   return true;
-  //   console.log(true)
-  // }
-  // return false;
-  // console.log(false)
+//Right sensor = 60 degrees 
+//Left Sensor = 120 degrees 
+//Position calculated using the parametric equetion 
+// x = cx + r * cos(a)
+// y = cy + r * sin(a)
+AutonomousVehicle.prototype.PointInVehicle = function (SensorAngle){
+    var x = this.position.center.vectorX + (this.radius * Math.sin(SensorAngle));
+    var y = this.position.center.vectorY + (this.radius * Math.cos(SensorAngle)); 
+    return new Vector(x, y); 
+}
+//Normalize formula: https://stats.stackexchange.com/questions/70801/how-to-normalize-data-to-0-1-range 
+// 1) Calculating the intensity of light on each sensor as (constant)/(distance)^2 normalized to a value between 0 and 1.
+AutonomousVehicle.prototype.LightIntensity = function(sensor){
+  var sensorDistance = sensor.distance(this.lightSource.lightLocation) 
+  //var intensity = this.lightSource.luminosity / Math.pow(sensorDistance, 2);  
+  var intensity = this.lightSource.luminosity /sensorDistance;  
+  //Normalize  
+  var normalizedIntensity = (intensity / 1);
+  return normalizedIntensity; 
+  //return intensity; 
 }
 
-//Vehicle direction 
-AutonomousVehicle.prototype.whereAmIGoing = function () {
-  // return this.radius += this.circle.center.vectorX;
-}
-//Difference in vector distance = force for torque
-AutonomousVehicle.prototype.SensorDifference = function () {
-  let leftSensor = new Vector(this.circle.center.vectorX -  this.radius, this.circle.center.vectorY);
-  let left = leftSensor.distance(this.locationGoal);
-  //console.log(left)
+AutonomousVehicle.prototype.update = function () { 
+  this.PointInVehicle();   
+  var rightIntensity = this.LightIntensity(this.rightSensor); 
+  var leftIntensity = this.LightIntensity(this.leftSensor);   
+ //Apply this to the force on each of the opposite wheels.
+  this.rightWheel = leftIntensity; 
+  this.leftWheel = rightIntensity;   
+//Calculate the force and torque based on these two values.
+  if(this.rightWheel > this.leftWheel){
+      this.force = (this.rightWheel * this.acceleration);
+      this.torque = this.leftWheel;
+  } else {
+    this.force = (this.leftWheel  * this.acceleration);
+    this.torque = this.rightWheel;
+  }
 
-  var rightSensor = new Vector(this.circle.center.vectorX +  this.radius, this.circle.center.vectorY);
-  let right = rightSensor.distance(this.locationGoal);
-  //console.log(right)
-  //console.log( Math.abs(left - right));
-  return Math.abs(left - right);
+  console.log("Force: " +  this.force);
+  console.log("Torque: " +  this.torque);
+
+  //Update the position and direction of the vehicle. 
+  this.position.center.addValue(this.force); 
+
+  //add torque to the angle, then calculate new direction
+  this.angle += this.torque;
+  this.direction = this.PointInVehicle(this.angle);
+ // console.log("New Position: " + this.position.center.vectorX + " - " +  this.position.center.vectorY)  
 }
 
-AutonomousVehicle.prototype.update = function () {
-  //let torqueForce = this.SensorDifference(); //Torque
-  var distance = this.speed * this.game.clockTick; 
-  var newPosition = new Vector(distance, distance);
-  newX =  this.circle.center.getX += distance;
-  this.circle.center.vectorX += distance;
-  this.circle.center.vectorY += distance;
-  //this.direction = (this.circle.center.vectorY + (-this.radius )); 
-  this.torque = torqueForce * this.radius * Math.sin(this.angle); // radius.x * force.y - radius.y * force.x;
-  //try to get closer to the light .. longer sensor? 
-}
-//Passing the Canvas width and height to adapt the vehicles
-// dimensions. 
-AutonomousVehicle.prototype.draw = function () {
-  this.makeCircle(this.circle.center.vectorX, this.circle.center.vectorY, this.radius, 'red');
-  this.ctx.moveTo(this.circle.center.vectorX, this.circle.center.vectorY);
+AutonomousVehicle.prototype.draw = function () {  
   this.ctx.lineWidth = 3;
-  this.ctx.lineTo(this.circle.center.vectorX, this.circle.center.vectorY + (-this.radius ));
+  this.makeCircle(this.position.center.vectorX, this.position.center.vectorY, this.radius, 'red');
+  this.ctx.moveTo(this.position.center.vectorX, this.position.center.vectorY);
+  //this.ctx.lineTo(this.position.center.vectorX, this.position.center.vectorY + (-this.radius ));
+  this.ctx.lineTo( this.direction.vectorX, this.direction.vectorY); 
   this.ctx.stroke();
 }
 AutonomousVehicle.prototype.makeCircle = function(x, y, size, color) {
@@ -140,7 +192,9 @@ AutonomousVehicle.prototype.makeCircle = function(x, y, size, color) {
   this.ctx.fillStyle = color;
   this.ctx.fill();
 }
-// the "main" code begins here
+/***********************************************************************
+ * Main code
+ **********************************************************************/
 var ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
